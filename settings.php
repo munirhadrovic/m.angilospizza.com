8c5a3f02ed5583fda62ce00b1333f48dfe1d707a
novicaa <novicamiketic@gmail.com>
2015-04-30 13:19:35 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,41 @@
+<?php
+	$adresa[0]="142 S. Broadway Blanchester, OH 45107"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[0]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[0]="Blanchestere";
+	$telephone[0]=9377833777;// Broj koji se poziva na dugme CALL US
+
+	$adresa[1]="9956 Harrison Ave.Harrison, OH 45030"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[1]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[1]="Harrison";
+	$telephone[1]=3674992;// Broj koji se poziva na dugme CALL US
+
+	$adresa[2]="6953 Harrison Ave.Cincinnati, OH 45247"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[2]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[2]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[2]="Cincinnati";
+	$telephone[2]=5135740777;// Broj koji se poziva na dugme CALL US
+
+	$adresa[3]="1723 Highway 28 Goshen, OH 45122"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[3]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[3]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[3]="Goshen";
+	$telephone[3]=5137221199;// Broj koji se poziva na dugme CALL US
+
+	$adresa[4]="304P AA Highway California, KY 41007"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[4]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[4]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[4]="AA Highway";
+	$telephone[4]=8596358700;// Broj koji se poziva na dugme CALL US
+
+	$adresa[5]="329 Pike St Covington, KY 41011"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[5]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[5]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[5]="Covington";
+	$telephone[5]=8592918111;// Broj koji se poziva na dugme CALL US
+
+	$companynumber=6;
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="turn"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup", "turn", "slidefade", "flow"
+?>
